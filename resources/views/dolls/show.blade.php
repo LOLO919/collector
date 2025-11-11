e286ae4290cc9a599620acacb2a6d34c7a0b89be
LOLO919 <lovetider@bk.ru>
2025-11-11 12:04:54 +0300

    главная страница

--- resources/views/dolls/show.blade.php
+++ resources/views/dolls/show.blade.php
@@ -1,6 +1,25 @@
-<div>
-    ID: {{ $doll->id }}
-    Name: {{ $doll->name }}
-    Description: {{ $doll->description }}
-    Photo: {{ $doll->photo }}
+<?php
+/** @var \App\Models\Doll $doll */
+?>
+
+@extends('layout')
+
+@section('content')
+<div class="block__73694 mb-2" id="services-section">
+    <div class="container">
+        <div class="row d-flex no-gutters align-items-stretch">
+{{-- #TODO /storage/$doll->photo storage_path() не смотрит в storage в проекте --}}
+            <div class="col-12 col-lg-6 block__73422" style="background-image: url('{{ "/storage/$doll->photo" }}');" data-aos="fade-right" data-aos-delay="">
+            </div>
+            <div class="col-lg-5 ml-auto p-lg-5 mt-4 mt-lg-0" data-aos="fade-left" data-aos-delay="">
+                <h2 class="mb-3 text-black">{{ $doll->name }}</h2>
+                <ul class="ul-check primary list-unstyled mt-5">
+                    <li>Год выпуска {{ $doll->year_of_issue }}</li>
+                    <li>Тираж {{ $doll->edition }}</li>
+                </ul>
+                <p>{{ $doll->description }}</p>
+            </div>
+        </div>
+    </div>
 </div>
+@endsection
